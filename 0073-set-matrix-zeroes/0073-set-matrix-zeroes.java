788bf0090e86311a7d417fe70cbeec0f5ebc68d6
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-05-21 01:36:03 +0530

    Time: 1 ms (72.83%), Space: 45.6 MB (82.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,27 +2,42 @@ public class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean[][] zeroes = new boolean[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroes[i][j] = true;
-                }
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (zeroes[i][j]) {
-                    for (int k = 0; k < n; k++) {
-                        matrix[i][k] = 0;
+        int colZero = 1;
+        //to keep track of zeroes we will take 1st row and 1st column
+        // {to save space of extra arrays}
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
                     }
-                    for (int k = 0; k < m; k++) {
-                        matrix[k][j] = 0;
+                    else{
+                        colZero=0;
                     }
                 }
             }
         }
+        //iterating without 1st row and 1st column {to avoid the conflict}
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                  if(matrix[i][0]==0 || matrix[0][j]==0){
+                      matrix[i][j]=0;
+                }
+                }
+                
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        } 
+        if(colZero==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            } 
+        }       
     }
 }
