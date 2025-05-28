b21844dd63062467339135a95a37fc217146d740
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-05-28 15:45:26 +0530

    Time: 4 ms (59.15%), Space: 50.2 MB (26.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,16 +2,34 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
         List<Integer> majority = new ArrayList<>();
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<n;i++){
-           hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
-        }
-        int mcnt=n/3;
-        for(Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            if(entry.getValue() > mcnt) {
-                majority.add(entry.getKey());
+
+        int cnt1 = 0, cnt2 = 0;
+        Integer ele1 = null, ele2 = null;
+        for (int num : nums) {
+            if (ele1 != null && num == ele1) {
+                cnt1++;
+            } else if (ele2 != null && num == ele2) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                ele1 = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                ele2 = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int num : nums) {
+            if (ele1 != null && num == ele1) cnt1++;
+            if (ele2 != null && num == ele2) cnt2++;
+        }
+        if (cnt1 > n / 3) majority.add(ele1);
+        if (ele2 != null && !ele2.equals(ele1) && cnt2 > n / 3) majority.add(ele2);
+
         return majority;
     }
 }
