b07fcacb8bfa16241b9f984f2a78687b22bd3aec
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-05-31 00:54:25 +0530

    Time: 18 ms (62.1%), Space: 44 MB (49.25%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,33 +3,30 @@ class Solution {
        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int left = j + 1;
-                int right = n - 1;
-
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if (sum == target) {
-                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-
-                        left++;
-                        right--;
-                    } else if (sum < target) {
-                        left++; 
-                    } else {
-                        right--; 
-                    }
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l = n-1;
+                while(k<l){
+                   long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                   if(sum==target){
+                     res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                     while(k<l && nums[k]==nums[k + 1]) k++;
+                     while(k<l && nums[l]==nums[l - 1]) l--;
+                     k++;
+                     l--;
+                   }
+                   else if(sum<target){
+                       k++;
+                   }
+                   else {
+                    l--;
+                   }
                 }
             }
         }
-
         return res;
     }
 }
