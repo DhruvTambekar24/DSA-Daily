dfbc83f53f63aba963e3571e387eec59ec5fee85
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-06-11 14:07:48 +0530

    Time: 42 ms (80.47%), Space: 55.3 MB (38.07%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,36 +1,43 @@
 class Solution {
     public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1);
+        return mergeSort(nums, new int[nums.length], 0, nums.length - 1);
     }
-    public int mergeSort(int[] nums, int left, int right) {
-        if (left >= right) return 0;
 
-        int mid = left + (right - left) / 2;
-        int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+    private int mergeSort(int[] arr, int[] temp, int left, int right) {
+        if (left >= right) return 0;
+        
+        int mid = (left + right) / 2;
+        int count = mergeSort(arr, temp, left, mid);
+        count += mergeSort(arr, temp, mid + 1, right);
         int j = mid + 1;
         for (int i = left; i <= mid; i++) {
-            while (j <= right && (long)nums[i] > 2 * (long)nums[j]) {
+            while (j <= right && (long) arr[i] > 2L * arr[j]) {
                 j++;
             }
             count += (j - (mid + 1));
         }
-        merge(nums, left, mid, right);
+        merge(arr, temp, left, mid, right);
         return count;
     }
-    public void merge(int[] nums, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
+
+    private void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
         while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) {
-                temp[k++] = nums[i++];
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
             } else {
-                temp[k++] = nums[j++];
+                temp[k++] = arr[j++];
             }
         }
-        while (i <= mid) temp[k++] = nums[i++];
-        while (j <= right) temp[k++] = nums[j++];
-        for (int l = 0; l < temp.length; l++) {
-            nums[left + l] = temp[l];
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+
+        for (int m = left; m <= right; m++) {
+            arr[m] = temp[m];
         }
     }
 }
