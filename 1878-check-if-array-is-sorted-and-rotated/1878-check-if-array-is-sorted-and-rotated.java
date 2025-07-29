5f1e76a3728c32e27e89b79c718de146feddd9cc
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-07-29 22:36:14 +0530

    Time: 0 ms (100%), Space: 41.4 MB (37.95%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean check(int[] nums) {
-      int n = nums.length;
-        for (int r = 0; r < n; r++) {
-            boolean isSorted = true;
-            for (int i = 0; i < n - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    isSorted = false;
-                    break;
+        int n = nums.length;
+        int rotationPoint = -1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (rotationPoint != -1) {
+                    return false; 
                 }
+                rotationPoint = i + 1;
             }
-            if (isSorted) return true;
-            int temp = nums[0];
-            for (int i = 1; i < n; i++) {
-                nums[i - 1] = nums[i];
-            }
-            nums[n - 1] = temp;
         }
-        return false;
+        if (rotationPoint == -1) return true;
+        for (int i = rotationPoint; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) return false;
+        }
+        for (int i = 0; i < rotationPoint - 1; i++) {
+            if (nums[i] > nums[i + 1]) return false;
+        }
+        if (nums[n - 1] > nums[0]) return false;
+
+        return true;
     }
 }
