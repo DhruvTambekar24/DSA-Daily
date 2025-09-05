89ea24aafafeddd95db68b0c5ccc4d5d098a0479
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-09-05 21:34:33 +0530

    Time: 0 ms (100%), Space: 40.9 MB (44.62%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
@@ -3,9 +3,9 @@ class Solution {
         return helper(num1, num2, 1);
     }
     private int helper(long num1, long num2, int ops) {
-         if (ops > 60) return -1;
+         if(ops > 60) return -1;
          long val = num1 - (long)num2 * ops;
-         if (val >= ops && Long.bitCount(val) <= ops) {
+         if(val >= ops && Long.bitCount(val) <= ops) {
            return ops;
         }
         return helper(num1, num2, ops + 1);
