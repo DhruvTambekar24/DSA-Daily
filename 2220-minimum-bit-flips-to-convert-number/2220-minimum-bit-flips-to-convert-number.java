378e9a16c3af95f9c080f6b97ad1303de9bb564c
Dhruv Tambekar <d.tambekar24@gmail.com>
2026-05-25 12:34:24 +0530

    Time: 0 ms (100%), Space: 42.3 MB (33.07%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,21 +1,10 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        while(start!=0){
-            l1.add(start%2);
-            start=start/2;
-        }
-        while(goal!=0){
-            l2.add(goal%2);
-            goal=goal/2;
-        }
+        int res = start ^ goal;
         int cnt=0;
-        int ms = Math.max(l1.size(),l2.size());
-        for(int i=0;i<ms;i++){
-           int ele1=(i<l1.size()) ? l1.get(i):0;
-           int ele2=(i<l2.size()) ? l2.get(i):0;
-            if(ele1!=ele2) cnt++;
+        for(int i=0;i<31;i++){
+            cnt = cnt+(res & 1);
+            res = res >> 1;
         }
         return cnt;
     }
