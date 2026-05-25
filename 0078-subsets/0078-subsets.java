935282d034a60b8224cc41ba1e55c8d9659c116f
Dhruv Tambekar <d.tambekar24@gmail.com>
2026-05-25 14:32:58 +0530

    Time: 1 ms (89.06%), Space: 44.1 MB (56.38%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,20 @@
 class Solution {
-    public void subSets(int idx,int[] nums,int n,List<List<Integer>> res,List<Integer> ss){
-       if(idx==n){
-        res.add(new ArrayList<>(ss));
-        return;
-       }
-       //not pick
-       subSets(idx+1,nums,n,res,ss);
-       //pick
-       ss.add(nums[idx]);
-       subSets(idx+1,nums,n,res,ss);
-       //removing
-       ss.remove(ss.size() - 1);
-    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new  ArrayList<>();
-        int n = nums.length;
-        subSets(0,nums,n,res,new ArrayList<>());
+        List<List<Integer>> res = new ArrayList<>();
+        subSets(0,nums,nums.length,res,new ArrayList<>());
         return res;
+    }
+
+    public void subSets(int idx, int[] nums, int n, List<List<Integer>> res, List<Integer> ss) {
+        int total=1<<n;
+        for(int i=0;i<total;i++){
+            List<Integer> subset = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i &(1<<j)) != 0){
+                    subset.add(nums[j]);
+                }
+            }
+            res.add(subset);
+        }
     }
 }
