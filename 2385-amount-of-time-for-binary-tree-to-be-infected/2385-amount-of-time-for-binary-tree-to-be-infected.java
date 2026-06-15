2ff656d57d6d38ff4caf41c51d422ec109412bc9
Dhruv Tambekar <d.tambekar24@gmail.com>
2026-06-15 20:02:02 +0530

    Time: 111 ms (29.93%), Space: 111.6 MB (68.21%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int amountOfTime(TreeNode root, int start) {
+        Map<TreeNode, TreeNode> parent=new HashMap<>();
+        TreeNode startNode=buildParent(root, parent, start);
+        Queue<TreeNode> q=new LinkedList<>();
+        Set<TreeNode> visited=new HashSet<>();
+        q.offer(startNode);
+        visited.add(startNode);
+        int time=-1;
+        while(!q.isEmpty()){
+            int size=q.size();
+            time++;
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.poll();
+                if(curr.left!=null && !visited.contains(curr.left)){
+                    q.offer(curr.left);
+                    visited.add(curr.left);
+                }
+                if(curr.right!=null && !visited.contains(curr.right)){
+                    q.offer(curr.right);
+                    visited.add(curr.right);
+                }
+                if(parent.get(curr)!=null && !visited.contains(parent.get(curr))){
+                    q.offer(parent.get(curr));
+                    visited.add(parent.get(curr));
+                }
+            }
+        }
+        return time;
+    }
+    private TreeNode buildParent(TreeNode root, Map<TreeNode, TreeNode> parent, int start) {
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        TreeNode startNode=null;
+        while(!q.isEmpty()){
+            TreeNode curr=q.poll();
+            if(curr.val==start){
+                startNode=curr;
+            }
+            if(curr.left!=null){
+                parent.put(curr.left, curr);
+                q.offer(curr.left);
+            }
+            if(curr.right!=null){
+                parent.put(curr.right, curr);
+                q.offer(curr.right);
+            }
+        }
+        return startNode;
+    }
+    
+}
