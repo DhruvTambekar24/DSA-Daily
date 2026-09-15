700f14dc21fcad2642b4cef25a18adbc7f109bd7
Dhruv Tambekar <d.tambekar24@gmail.com>
2026-09-15 16:03:35 +0530

    Time: 5 ms (98.99%), Space: 45.3 MB (38.52%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,20 +1,29 @@
 class Solution {
-    public void findCombinations(int ind,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
-        if(target==0){
-            ans.add(new ArrayList<>(ds));
-        }
-        for(int i=ind;i<arr.length;i++){
-            if(i>ind && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            ds.add(arr[i]);
-            findCombinations(i+1,arr,target-arr[i],ans,ds);
-            ds.remove(ds.size()-1);
-        }
-    }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         Arrays.sort(candidates);
-        findCombinations(0,candidates,target,res,new ArrayList<>());
+        solve(candidates,target,0,list,res);
         return res;
+    }
+    public void solve(int[] candidates,int target,int i,List<Integer> list,List<List<Integer>> res) {
+        if(target==0){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        if(target<0 || i==candidates.length){
+            return;
+        }
+        for(int j=i;j<candidates.length;j++){
+            if(j>i && candidates[j]==candidates[j-1]){
+                continue;
+            }
+            if(candidates[j]>target){
+                break;
+            }
+            list.add(candidates[j]);
+            solve(candidates,target-candidates[j],j+1,list,res);
+            list.remove(list.size()-1);
+        }
     }
 }
