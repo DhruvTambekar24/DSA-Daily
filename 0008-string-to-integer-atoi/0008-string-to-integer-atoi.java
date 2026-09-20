02dc80676f5de87266dbb5c945931983c4613150
Dhruv Tambekar <d.tambekar24@gmail.com>
2026-09-20 18:13:35 +0530

    Time: 1 ms (100%), Space: 43.8 MB (39.53%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s==null) return 0;
-        s = s.trim();
-        if(s.length()==0) return 0;
-        int sign = 1;
-        int i = 0;
-        if(s.charAt(0) == '-'){
+        s=s.trim();
+        int sign=1;
+        int i=0;
+        int num=0;
+        if(s.length()==0){
+            return 0;
+        }
+        if(s.charAt(0)=='-'){
             sign=-1;
             i++;
-        } else if(s.charAt(0) == '+'){
+        }
+        else if(s.charAt(0)=='+'){
             i++;
         }
-        return helper(s,i,0,sign);
-    }
-    private int helper(String s,int i,long num,int sign){
-        if(i>=s.length()){
-            return (int)(sign*num);
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+            int digit=s.charAt(i)-'0';
+            if(num>(Integer.MAX_VALUE-digit)/10){
+                if(sign==1){
+                    return Integer.MAX_VALUE;
+                }else{
+                    return Integer.MIN_VALUE;
+                }
+            }
+            num=num*10+digit;
+            i++;
         }
-        char c=s.charAt(i);
-        if (!Character.isDigit(c)) {
-            return (int)(sign*num);
-        }
-        int digit =c-'0';
-        num = num*10+digit;
-        if(sign==1 && num>Integer.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }
-        if(sign ==-1 && -num<Integer.MIN_VALUE){
-            return Integer.MIN_VALUE;
-        }
-        return helper(s,i+1,num,sign);
+        return num*sign;
     }
 }
