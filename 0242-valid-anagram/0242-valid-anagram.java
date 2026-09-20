8301b4641135b655d1126d338818b22bca5718f8
Dhruv Tambekar <d.tambekar24@gmail.com>
2026-09-20 12:44:31 +0530

    Time: 5 ms (63.11%), Space: 44.3 MB (81.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,13 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int n=s.length();
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] count=new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
         }
-        for(int i=0;i<n;i++){
-            char c=t.charAt(i);
-            if(!map.containsKey(c)){
-                return false;
-            }
-            map.put(c,map.get(c)-1);
-            if(map.get(c)<0){
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
                 return false;
             }
         }
